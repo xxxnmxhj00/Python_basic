f327e12f81ad42f40df4880f4c0c24d5dafa9742
hibar6123 <hibar6123@gmail.com>
2024-12-28 12:28:58 +0900

    - 01

---
+++ ex01/sam03.py
@@ -0,0 +1,15 @@
+# 산술 연산자 : +, -, *, 나누기 : /, 나머지 : %, 몫 : //, **(누승)
+
+n1 = 100
+n2 = 20
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+
+div = n1 / n2
+div2 = n1 % n2
+div3 = n2 // n2
+
+print('add:', add, 'sub:', sub, 'mul:', mul)
+
+#  ijj
